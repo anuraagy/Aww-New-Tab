622f8f1b585272f100fd5d2ad35f835a2a6f0fd0
Dhananjay Nakrani <dhananjaynakrani@gmail.com>
2017-05-14 18:33:12 -0700

    Improve extension
    In particular,
    1. Add a localStorage cache for top awws data.
    2. Make it work for almost all types of images and formats.
    3. Make it robust enough to handle imgur images.
    4. Visual improvements (dont cover full screen, show link in title)

--- scripts.js
+++ scripts.js
@@ -1,25 +1,77 @@
+// Cache time-to-live in milliseconds.
+const CACHE_TTL = 20 * 60 * 1000; // 20 minutes
 
-function changeVideo() {
-	 var x = Math.floor((Math.random() * 24) + 1);
-	 $.ajax({
-	     url: "http://www.reddit.com/r/aww/top/.json",
-	     dataType: "json",
-	     success: function(data) {
-	     	console.log(data.data.children[x].data.url);
-	     	if((data.data.children[x].data.url).includes("gifv")) {
-	     		var url = data.data.children[x].data.url;
-	     		var point = data.data.children[x].data;
-	 			url = url.substring(0,url.indexOf("gifv")-1) + ".mp4";
-	 			console.log(url);
-	     		document.getElementById("video").src = url;
-	     		document.getElementById("title").innerHTML = point.title;
-	     	}else{
-	     		changeVideo();
-	     	}
-	     }
+const CACHE_DATA_KEY = 'r_aww_top';
+const CACHE_TS_KEY = 'r_aww_top_ts';
+
+function setImage(url, title, permalink) {
+	var elem = "";
+	if (url.includes("gifv")) {
+		elem = $("#video");
+		url = url.substring(0, url.indexOf("gifv")-1) + ".mp4";
+	} else {
+		elem = $("#image");
+	}
+	elem[0].src = url;
+	elem.toggleClass('hidden');
+	var titleElem = $("#title")[0];
+	titleElem.innerHTML = title;
+	titleElem.href = 'http://reddit.com/' + permalink;
+}
+
+function setRandomImage(data) {
+	var x = Math.floor((Math.random() * 24) + 1);
+	var randomChild = data.data.children[x];
+	var point = randomChild.data;
+	var url = point.url;
+	if (url.includes("imgur.com") && !url.includes("i.imgur.com")) {
+		// imgur images require an extra hop to get the actual image.
+		var elems = url.split('/');
+		imgurApiUrl = 'https://api.imgur.com/3/image/' + elems[elems.length-1];
+		$.ajax({
+			url: imgurApiUrl,
+			dataType: "json",
+			success: function(data) {
+				setImage(data.data.link, point.title, point.permalink);
+			},
+		 });
+	} else {
+		setImage(url, point.title, point.permalink);
+	}
+	return "";
+}
+
+function changeImage() {
+	// Check local storage for top awws.
+	var cache = localStorage.getItem(CACHE_DATA_KEY);
+	var cache_ts = localStorage.getItem(CACHE_TS_KEY);
+	var now_ts = (new Date().getTime());
+	if (cache && cache_ts && cache.length > 0 && cache_ts.length > 0 && (now_ts - parseInt(cache_ts) <= CACHE_TTL)) {
+		// Cache hit.
+		var err = setRandomImage(JSON.parse(cache));
+		if (err.length > 0) {
+			console.log(err);
+		}
+		return;
+	}
+
+	// Cache miss or expired.
+	$.ajax({
+		url: "http://www.reddit.com/r/aww/top/.json",
+		dataType: "json",
+		success: function(data) {
+			var err = setRandomImage(data);
+			if (err.length > 0) {
+				console.log(err);
+				return;
+			}
+			// Store in cache.
+			localStorage.setItem(CACHE_TS_KEY, (new Date().getTime()));
+			localStorage.setItem(CACHE_DATA_KEY, JSON.stringify(data));
+		},
 	 });
 }
 
 window.onload = function() {
-	changeVideo();
+	changeImage();
 }
